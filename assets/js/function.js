82fcc03e1aeb0a1869b002978f9c03d0a39cddeb
ma30nousagi <punchkids3713+ma3onousagi@gmail.com>
2021-05-21 10:48:16 +0900

    top-lead完了

--- assets/js/function.js
+++ assets/js/function.js
@@ -1,179 +1,22 @@
-// $(document).ready(function () {
-//     hsize = $(window).height();
-//     $(".hero").css("height", hsize + "px");
-//   });
-//   $(window).resize(function () {
-//     hsize = $(window).height();
-//     $(".hero").css("height", hsize + "px");
-// });
+/*===========================================================*/
+/*　MENUクリックしたらナビ出現*/
+/*===========================================================*/
 
-
-  //.headerに.activeを付与
-$(window).on("scroll", function(){
-    if( $(window).scrollTop() > 50 ){
-        $('.header').addClass('active');
-        }else{
-        $('.header').removeClass('active');
-    }
+$(".openbtn").click(function () {//ボタンがクリックされたら
+	$(this).toggleClass('active');//ボタン自身に activeクラスを付与し
+    $("#g-nav").toggleClass('panelactive');//ナビゲーションにpanelactiveクラスを付与
 });
 
-//page-topボタン
-$(window).on("scroll" ,function(){
-    if( $(window).scrollTop() > 100){
-        $('.page-top').fadeIn();
-    }else{
-        $('.page-top').fadeOut();
-    }
-});
-
-
-//ハンバーガーメニュー
-$('.btn-menu').on('click', function(){
-    $(this).toggleClass('active');
-    $('.global-nav').toggleClass('show');
-})
-//ハンバーガーメニュー2
-$(".openbtn").click(function() {
-  $(this).toggleClass('active');
-  $(".global-nav").toggleClass('panelactive')
-});
-
-
-//ヘッダーの高さ分だけコンテンツを下げる
-$(function() {
-    let height=$("#header").height();
-    $("main").css("margin-top", height+20);//mainにmargin-top(header分)+20pxだけ余裕をもたせる
+$("#g-nav a").click(function () {//ナビゲーションのリンクがクリックされたら
+    $(".openbtn").removeClass('active');//ボタンの activeクラスを除去し
+    $("#g-nav").removeClass('panelactive');//ナビゲーションのpanelactiveクラスも除去
 });
 
 
 
-
-// let _window = $(window),
-//     _header = $('.header'),
-//     heroBottom;
- 
-// _window.on('scroll',function(){
-//     heroBottom = $('.desc').height();
-//     if(_window.scrollTop() > heroBottom){
-//         _header.addClass('transform');   
-//     }
-//     else{
-//         _header.removeClass('transform');   
-//     }
-// });
- 
-// _window.trigger('scroll');
-
-
-// swiper 
-let mySwiper = new Swiper ('.swiper-container', {
-    autoplay: {
-      delay: 2000,
-    },
-    loop: true,  //ループ可能（ループモードを有効に）
-    slidesPerView: 'auto',  //スライドを2つ（分）表示
-    grabCursor: true,
-    centeredSlides : true,  //アクティブなスライドを中央に表示
-    effect: 'coverflow',  //スライドのエフェクトを coverflow に
-    coverflowEffect: {
-      rotate: 50,
-      stretch: 0,
-      depth: 100,
-      modifier: 1,
-      slideShadows: true,
-    },
-    // デフォルトの設定
-    slidesPerView: 1,
-    spaceBetween: 0,
-    
-    // レスポンシブブレーポイント（画面幅による設定）
-    breakpoints: {
-      // 画面幅が 600px 以上の場合（window width >= 600px）
-      600: {
-        slidesPerView: 2,
-        spaceBetween: 5
-      },
-      // 画面幅が 960px 以上の場合（window width >= 960px）
-      960: {
-        slidesPerView: 3,
-        spaceBetween: 10
-      }
-    },
-    
-    pagination: {  //ページネーションを表示
-      el: '.swiper-pagination',
-      clickable: true,  //アイコンをクリックすると対応するスライドに移動
-    },
-   
-    navigation: {  //ナビゲーションボタンを表示
-      nextEl: '.swiper-button-next',
-      prevEl: '.swiper-button-prev',
-    },
-})
-
-
-//wow.js
-new WOW().init();
-
-
-//sideGnav
-$(window).on("scroll" ,function(){
-  if( $(window).scrollTop() > 200){
-      $('.sideGnav').fadeIn();
-  }else{
-      $('.sideGnav').fadeOut();
-  }
-});
-
-
-//IntersectionObserver
-document.addEventListener('DOMContentLoaded', function () {
-
-  const cb = function (el, isIntersecting) {
-      if(isIntersecting) {
-          el.classList.add('inview');
-      }
-  } 　
-
-  const so = new ScrollObserver('.cover-slide', cb);
-});
-
-
-//gallery 
-function fadeAnime(){
-// flipLeft
-$('.gallery li').each(function(){ 
-    var elemPos = $(this).offset().top;
-    var scroll = $(window).scrollTop();
-    var windowHeight = $(window).height();
-    if (scroll >= elemPos - windowHeight){
-        $(this).addClass('flipLeft');
-    }else{
-        $(this).removeClass('flipLeft');
-    }
-});
-}
-
-
-$(window).scroll(function (){
-  fadeAnime();/* アニメーション用の関数を呼ぶ*/
-});
-
-
-//アコーディオンをクリックした時の動作
-$('.accordion__title').on('click', function() {//タイトル要素をクリックしたら
-  var findElm = $(this).next(".accordion__box");//直後のアコーディオンを行うエリアを取得し
-  $(findElm).slideToggle();//アコーディオンの上下動作
-    
-  if($(this).hasClass('close')){//タイトル要素にクラス名closeがあれば
-    $(this).removeClass('close');//クラス名を除去し
-  }else{//それ以外は
-    $(this).addClass('close');//クラス名closeを付与
-  }
-});
-
-
-
+/*===========================================================*/
+/*　splash-logo */
+/*===========================================================*/
 $(window).on('load',function(){
   $("#splash-logo").delay(1200).fadeOut('slow');//ロゴを1.2秒でフェードアウトする記述
 
