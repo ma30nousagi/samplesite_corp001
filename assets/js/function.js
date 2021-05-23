66b2514019d1e1028833a53dca82ecf740d70854
ma30nousagi <punchkids3713+ma3onousagi@gmail.com>
2021-05-24 06:28:23 +0900

    TOP完成

--- assets/js/function.js
+++ assets/js/function.js
@@ -1,5 +1,5 @@
 /*===========================================================*/
-/*　MENUクリックしたらナビ出現*/
+/*　openbtn→g-nav　*/
 /*===========================================================*/
 
 $(".openbtn").click(function () {//ボタンがクリックされたら
@@ -30,4 +30,52 @@ $(window).on('load',function(){
   
   //=====ここまで背景が伸びた後に動かしたいJSをまとめる
       
+});
+
+/*===========================================================*/
+/*　footer accordion */
+/*===========================================================*/
+$('.c-footer-heading').on('click', function() {//タイトル要素をクリックしたら
+	$('.box').slideUp(500);//クラス名.boxがついたすべてのアコーディオンを閉じる
+    
+	let findElm = $(this).next(".box");//タイトル直後のアコーディオンを行うエリアを取得
+    
+	if($(this).hasClass('close')){//タイトル要素にクラス名closeがあれば
+		$(this).removeClass('close');//クラス名を除去    
+	}else{//それ以外は
+		$('.close').removeClass('close'); //クラス名closeを全て除去した後
+		$(this).addClass('close');//クリックしたタイトルにクラス名closeを付与し
+		$(findElm).slideDown(500);//アコーディオンを開く
+	}
+  
+});
+
+
+/*===========================================================*/
+/*　page-top */
+/*===========================================================*/
+function PageTopAnime() {
+	let scroll = $(window).scrollTop();
+	if (scroll >= 250){//上から200pxスクロールしたら
+		$('#page-top').removeClass('DownMove');//#page-topについているDownMoveというクラス名を除く
+		$('#page-top').addClass('UpMove');//#page-topについているUpMoveというクラス名を付与
+	}else{
+		if($('#page-top').hasClass('UpMove')){//すでに#page-topにUpMoveというクラス名がついていたら
+			$('#page-top').removeClass('UpMove');//UpMoveというクラス名を除き
+			$('#page-top').addClass('DownMove');//DownMoveというクラス名を#page-topに付与
+		}
+	}
+}
+
+// 画面をスクロールをしたら動かしたい場合の記述
+$(window).scroll(function () {
+	PageTopAnime();/* スクロールした際の動きの関数を呼ぶ*/
+});
+
+// #page-topをクリックした際の設定
+$('#page-top a').click(function () {
+    $('body,html').animate({
+        scrollTop: 0//ページトップまでスクロール
+    }, 500);//ページトップスクロールの速さ。数字が大きいほど遅くなる
+    return false;//リンク自体の無効化
 });
